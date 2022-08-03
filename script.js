74da1b3ca6cd8a4438311e0ae018dccead6c4f87
eco-can <110513464+eco-can@users.noreply.github.com>
2022-08-02 23:33:00 -0400

    upload from english version

---
+++ script.js
@@ -0,0 +1,6 @@
+var points = 400;
+
+function updateText() {
+document.getElementById("exchangeText").innerHTML = "Exchanged! Gift card code: TESTCODE123";
+document.getElementById("pointCounter").innerHTML = "My account points: 0";
+}
